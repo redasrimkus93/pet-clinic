b203555e79461a2621ce4464540c6dc5286f6d89
Redas <redas.rimkus93@gmail.com>
2020-10-16 00:26:05 +0300

    Added Vets list and updated controller

--- pet-clinic-data/src/main/java/springframework/petclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/springframework/petclinic/services/map/AbstractMapService.java
@@ -1,12 +1,13 @@
 package springframework.petclinic.services.map;
 
-import springframework.petclinic.model.BaseEntity;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
-import java.util.*;
+public abstract class AbstractMapService<T, ID> {
 
-public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
-
-    protected Map<Long, T> map = new HashMap<>();
+    protected Map<ID, T> map = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -17,12 +18,6 @@ public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> 
     }
 
     T save(ID id, T object){
-        if(object != null){
-            if (object.getId() == null){
-                object.setId(getNextId());
-            }
-            map.put(object.getId(), object);
-        }
         map.put(id, object);
 
         return object;
@@ -34,9 +29,5 @@ public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> 
 
     void delete(T object){
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
-    }
-
-    private Long getNextId(){
-        return Collections.max(map.keySet()) + 1;
     }
 }
--- pet-clinic-web/src/test/java/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/petclinic/PetClinicApplicationTests.java
@@ -1,9 +1,13 @@
 package petclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import springframework.petclinic.PetClinicApplication;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = PetClinicApplication.class)
 class PetClinicApplicationTests {
 
     @Test
