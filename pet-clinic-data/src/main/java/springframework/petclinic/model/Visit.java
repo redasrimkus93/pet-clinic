80a9f7c4b5aa7fae22b89d81244c33a9d9a2924b
Redas <redas.rimkus93@gmail.com>
2020-10-21 22:58:25 +0300

    Added Lombok

--- pet-clinic-data/src/main/java/springframework/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/springframework/petclinic/model/Visit.java
@@ -1,9 +1,20 @@
 package springframework.petclinic.model;
 
+import lombok.*;
+
+import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "vets")
 public class Visit extends BaseEntity {
 
     private LocalDate date;
@@ -13,27 +24,4 @@ public class Visit extends BaseEntity {
     @JoinColumn(name = "pet_id")
     private Pet pet;
 
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Pet getPet() {
-        return pet;
-    }
-
-    public void setPet(Pet pet) {
-        this.pet = pet;
-    }
 }
