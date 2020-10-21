d94381859546d219d11faf266eecc50313b0242f
Redas <redas.rimkus93@gmail.com>
2020-10-21 21:28:02 +0300

    Added spring data JPA service for PetType

--- pet-clinic-data/src/main/java/springframework/petclinic/repositories/PetTypeRepository.java
+++ pet-clinic-data/src/main/java/springframework/petclinic/repositories/PetTypeRepository.java
@@ -1,6 +1,7 @@
 package springframework.petclinic.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import springframework.petclinic.model.PetType;
 
-public interface PetTypeRepository extends CrudRepository {
+public interface PetTypeRepository extends CrudRepository<PetType, Long> {
 }
